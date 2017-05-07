99513dfdb695f988b600dec8d4b339b8fe6a70b4
winhow <1091033487@qq.com>
2017-05-07 21:47:47 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
+    private static final String REGEX = "\\bdog\\b";
+    private static final String INPUT = "dog dog dog doggie dogg";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(INPUT);    // 获得一个匹配器对象
+        int count = 0;
+        while(m.find()) {
+            count++;
+            System.out.println("Match number "+count);
+            System.out.println("start(): "+m.start());
+            System.out.println("end(): "+m.end());
+        }
     }
 }
